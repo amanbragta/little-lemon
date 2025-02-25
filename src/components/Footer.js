f0ba5b6e098e82e4ac3ab47c83c6acadee2180dc
amanbragta <amanbragta@gmail.com>
2025-02-25 14:04:34 +0530

    added semantic tags and assets

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import logoImage from "../assets/icons_assets/Logo.svg";
+export default function Footer() {
+  return (
+    <footer>
+      <img src={logoImage} alt="" />
+      Doormat Navigation Contact Social media links
+    </footer>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import logoImg from "../assets/icons_assets/Logo .svg";
+export default function Header() {
+  return (
+    <header>
+      <img src={logoImg} alt="logo"></img>
+    </header>
+  );
+}
